c9701ac866ae58c02e373126262135e2c98c0c25
Fatima86711 <fatimazafar411133@gmail.com>
2025-05-10 00:51:48 +0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from teacher_dashboard import TeacherDashboard
-
-if __name__ == "__main__":
-    # For testing purposes, hardcode teacher_id (replace with actual login logic later)
-    teacher_id = "T001"
-    app = TeacherDashboard(teacher_id)
-    app.mainloop()
